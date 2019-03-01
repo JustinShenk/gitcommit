a34799b5d1c55647717751b659ca5cb303c467eb
Justin Shenk <shenkjustin@gmail.com>
2019-03-01 16:06:12 +0000

    Add gunicorn error handling

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,10 @@
+import logging
+
 from app import app
 import views
 
-if __name__=='__main__':
+if __name__== '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
     app.run(host='0.0.0.0')
